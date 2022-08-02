c1aba0c5735fc04dcf5bd227ec2be416259f50dd
Nidhi Sahu <Nidhi.Sahu@securemeters.com>
2022-08-02 16:48:07 +0530

    first commit

---
+++ Layoutlearning/Controllers/EmployeeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Layoutlearning.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+
+        [Route("Employees")]
+        public ActionResult GetList()
+        {
+            return View("GetList","_Layout");
+        }
+        [Route("NewEmployee")]
+        public ActionResult AddEmployee()
+        {
+            return View();
+        }
+    }
+}
